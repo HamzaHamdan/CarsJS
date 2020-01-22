300b551a0f5251c688b96c985c8c1e1aba9578e8
Kassandra Gonzalez <kgkassieg@gmail.com>
2020-01-21 20:15:21 -0600

    updated sql files

--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,32 @@
 CREATE DATABASE cars_db;
 USE cars_db;
 
+CREATE TABLE cars
+(
+    id int NOT NULL
+    AUTO_INCREMENT,
+	make varchar
+    (255) NOT NULL,
+	model varchar
+    (255) NOT NULL,
+    year varchar
+    (255) NOT NULL,
+    color varchar
+    (255) NOT NULL,
+    milage varchar
+    (255) NOT NULL,
+	PRIMARY KEY
+    (id)
+);
+
+CREATE TABLE images
+(
+    id int NOT NULL
+    AUTO_INCREMENT,
+    carVinNum varchar
+    (255) NOT NULL,
+    image varchar
+    (255) NOT NULL,
+	PRIMARY KEY
+    (id)
+);
