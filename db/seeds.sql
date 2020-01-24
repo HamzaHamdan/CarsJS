6a07f49406a1f5ebe82e3779e5e325b424b2e8af
Brynn Chapman <brynn_m_chapman@outlook.com>
2020-01-23 20:32:29 -0600

    Added models to database.

--- db/seeds.sql
+++ db/seeds.sql
@@ -38,5 +38,103 @@ values
     ((select id
         from carmakes
         where make = "BMW"), "528i");
+insert into carModels
+    (makeId, model)
+values
+    ((select id
+        from carmakes
+        where make = "Honda"), "Accord");
+insert into carModels
+    (makeId, model)
+values
+    ((select id
+        from carmakes
+        where make = "Honda"), "Civic");
+insert into carModels
+    (makeId, model)
+values
+    ((select id
+        from carmakes
+        where make = "Honda"), "Fit");
+        insert into carModels
+    (makeId, model)
+values
+    ((select id
+        from carmakes
+        where make = "Audi"), "Q5");
+insert into carModels
+    (makeId, model)
+values
+    ((select id
+        from carmakes
+        where make = "Audi"), "A3");
+insert into carModels
+    (makeId, model)
+values
+    ((select id
+        from carmakes
+        where make = "Audi"), "A6");
+insert into carModels
+    (makeId, model)
+values
+    ((select id
+        from carmakes
+        where make = "Mercedes"), "AMG GT");
+insert into carModels
+    (makeId, model)
+values
+    ((select id
+        from carmakes
+        where make = "Mercedes"), "Benz S-Class");
+insert into carModels
+    (makeId, model)
+values
+    ((select id
+        from carmakes
+        where make = "Mercedes"), "Benz G-Class");
+
+
+--Added models.--
+
+SELECT * FROM cars_db.carmodels;
+select * from carmodels;
+
+delete from carmodels where id in (5,6);
+commit;
+insert into carmodels(makeid,model)
+values ((select id from carmakes where make="Audi"),'Q5');
+
+insert into carmodels(makeid,model)
+values ((select id from carmakes where make="Audi"),'A3');
+
+insert into carmodels(makeid,model)
+values ((select id from carmakes where make="Honda"),'Accord');
+
+insert into carmodels(makeid,model)
+values ((select id from carmakes where make="Honda"),'Civic');
+
+insert into carmodels(makeid,model)
+values ((select id from carmakes where make="Honda"),'Fit');
+
+insert into carmodels(makeid,model)
+values ((select id from carmakes where make="Mercedes"),'Benz S-Class');
+
+insert into carmodels(makeid,model)
+values ((select id from carmakes where make="Mercedes"),'Benz G-Class');
+
+insert into carmodels(makeid,model)
+values ((select id from carmakes where make="Mercedes"),'AMG GT');
+
+
+
+
+
+
+
+select * from carmodels;
+DELETE FROM CARMAKES WHERE ID > 5;
+COMMIT;
+
+
 
 
